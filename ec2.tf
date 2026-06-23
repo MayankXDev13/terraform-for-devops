bf7aa0bd61e0ddbafb9aa39f03779ced2bb303ee
Mayank <vijaymaynk2003@gmail.com>
2026-06-23 23:34:48 +0530

    chore: remove unused ec2 instance configurations and add automate.txt to gitignore

--- ec2.tf
+++ ec2.tf
@@ -72,8 +72,6 @@ resource "aws_instance" "my-instance" {
   for_each = tomap({
     mayankxdev-micro  = "t2.micro",
     mayankxdev-medium = "t2.medium",
-    # mayankxdev-large  = "t2.large",
-    # mayankxdev-xlarge = "t2.xlarge"
   })
 
   depends_on             = [aws_security_group.my-sg, aws_key_pair.deployer]
