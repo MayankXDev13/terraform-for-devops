91564f362b0aa0a67f0525f56955e05dc4ea82a9
Mayank <vijaymaynk2003@gmail.com>
2026-07-16 16:32:45 +0530

    feat: update Terraform configuration for spot instances with improved security group and output descriptions

--- spot-ec2/main.tf
+++ spot-ec2/main.tf
@@ -1,62 +1,85 @@
-resource "aws_default_vpc" "default" {
-
+data "aws_vpc" "default" {
+  default = true
 }
 
 data "aws_subnets" "default" {
   filter {
-    name   = "default-for-az"
-    values = ["true"]
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
-resource "aws_security_group" "my_sg" {
-  name   = "spot-instance-sg-${var.env}"
-  vpc_id = aws_default_vpc.default.id
+resource "aws_security_group" "spot_sg" {
+  name        = "spot-instance-sg"
+  description = "Security group for spot instance"
+  vpc_id      = data.aws_vpc.default.id
+
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTPS
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "spot-instance-sg"
+  }
 }
 
 
 
 resource "aws_instance" "spot" {
-  ami                         = var.ec2_ami_id
-  instance_type               = "t3.medium"
-  subnet_id                   = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids      = [aws_security_group.my_sg.id]
-  associate_public_ip_address = true
-  key_name                    = "mayank"
+  ami                    = "ami-01a00762f46d584a1"
+  instance_type          = "t3.medium"
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.spot_sg.id]
 
-  root_block_device {
-    volume_type = var.ec2_root_volume_type
-    volume_size = var.env == "prod" ? 20 : var.ec2_default_root_storage_size
-  }
-
+  # Spot instance request
   instance_market_options {
     market_type = "spot"
-
     spot_options {
-      spot_instance_type             = "one-time"
+      spot_instance_type             = "one-time" # or "persistent"
       instance_interruption_behavior = "terminate"
     }
+  }
+
+  associate_public_ip_address = true
+
+  # Optional: add your key pair name
+  key_name = "mayank"
+
+  root_block_device {
+    volume_size           = 20 # GB
+    volume_type           = "gp3"
+    delete_on_termination = true
   }
 
   tags = {
--- spot-ec2/output.tf
+++ spot-ec2/output.tf
@@ -1,7 +1,19 @@
-output "public_ip" {
-  value = aws_instance.spot.public_ip
+output "instance_id" {
+  description = "Spot instance ID"
+  value       = aws_instance.spot.id
 }
 
-output "instance_id" {
-  value = aws_instance.spot.id
+output "public_ip" {
+  description = "Public IP of the spot instance"
+  value       = aws_instance.spot.public_ip
+}
+
+output "security_group_id" {
+  description = "Security group ID"
+  value       = aws_security_group.spot_sg.id
+}
+
+output "vpc_id" {
+  description = "Default VPC ID used"
+  value       = data.aws_vpc.default.id
 }
